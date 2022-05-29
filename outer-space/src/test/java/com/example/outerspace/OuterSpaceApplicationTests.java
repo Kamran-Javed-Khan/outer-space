31dd153192614f5d5e35d7928892b81f46a21e0a
Kamran-Javed-Khan <106434452+Kamran-Javed-Khan@users.noreply.github.com>
2022-05-29 12:39:00 +0530

    Add files via upload

---
+++ outer-space/src/test/java/com/example/outerspace/OuterSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outerspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
